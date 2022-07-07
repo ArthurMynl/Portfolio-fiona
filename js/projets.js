b00d2597af2d833fd0ef17caceef82fe83222954
ArthurMynl <61516506+ArthurMynl@users.noreply.github.com>
2022-07-07 18:13:29 +0200

    changement couleur

--- js/projets.js
+++ js/projets.js
@@ -1,6 +1,7 @@
 let slideIndex = [1, 1, 1, 1];
 /* Class the members of each slideshow group with different CSS classes */
 let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
+let dotId = ["dots1", "dots2", "dots3", "dots4"]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
@@ -14,12 +15,17 @@ function plusSlides(n, no) {
 function showSlides(n, no) {
     let i;
     let x = document.getElementsByClassName(slideId[no]);
+    let dots = document.getElementsByClassName(dotId[no]);
     if (n > x.length) { slideIndex[no] = 1 }
     if (n < 1) { slideIndex[no] = x.length }
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
     x[slideIndex[no] - 1].style.display = "flex";
+    dots[slideIndex[no] - 1].className += " active";
 }
 
 function currentSlide(n, no) {
